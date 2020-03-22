66c3f4bd28a6704100d37251fcf4df1107c81477
Joel Walker <joelwalker1995@gmail.com>
2020-03-22 01:56:05 -0500

    Much better coloring

--- src/shaders/main.frag.glsl
+++ src/shaders/main.frag.glsl
@@ -30,6 +30,17 @@ highp float rand(float seed) {
 }
 
 
+const bool blinn = true;
+const vec3 lightPos = vec3(0.0, 0.0, 10.0);
+const vec3 lightColor = vec3(1.0, 1.0, 1.0);
+const float lightPower = 40.0;
+const vec3 ambientColor = vec3(0.0, 0.0, 0.0);
+const vec3 diffuseColor = vec3(188/256.0,143/256.0,143/256.0);
+const vec3 specColor = vec3(1.0, 1.0, 1.0);
+const float shininess = 32.0;
+const float screenGamma = 2.2; // Assume the monitor is calibrated to the sRGB color space
+
+
 void main(void) {
     if (showTriangles) {
         if (gl_FrontFacing) {
@@ -42,33 +53,37 @@ void main(void) {
         vec3 yTangent = dFdy(orig_position);
         vec3 faceNormal = normalize(cross(xTangent, yTangent));
 
-        vec3 lightPos = vec3(0.0, 0.0, 100.0);
-        vec3 viewPos = eyePos;
-        float spec = 0.0;
-        bool blinn = true;
+        vec3 lightDir = lightPos - orig_position;
+        float distance = length(lightDir);
+        distance = distance * distance;
+        lightDir = normalize(lightDir);
 
-        vec3 color = vert_color;
-        // ambient
-        vec3 ambient = 0.05 * color;
-        // diffuse
-        vec3 lightDir = normalize(lightPos - orig_position);
-        float diff = max(dot(lightDir, faceNormal), 0.0);
-        vec3 diffuse = diff * color;
-        // specular
-        vec3 viewDir = normalize(viewPos - orig_position);
-        vec3 reflectDir = reflect(-lightDir, faceNormal);
-        if (blinn)
-        {
-            vec3 halfwayDir = normalize(lightDir + viewDir);
-            spec = pow(max(dot(faceNormal, halfwayDir), 0.0), 32.0);
-        }
-        else
-        {
+        float lambertian = max(dot(lightDir, faceNormal), 0.0);
+        float specular = 0.0;
+
+        if (lambertian > 0.0) {
+
+          vec3 viewDir = normalize(-orig_position);
+
+          if (blinn) {
+              vec3 halfDir = normalize(lightDir + viewDir);
+              float specAngle = max(dot(halfDir, faceNormal), 0.0);
+              specular = pow(specAngle, shininess);
+          } else {
             vec3 reflectDir = reflect(-lightDir, faceNormal);
-            spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
+            float specAngle = max(dot(reflectDir, viewDir), 0.0);
+            // note that the exponent is different here
+            specular = pow(specAngle, shininess/4.0);
+          }
         }
-        vec3 specular = vec3(0.3) * spec; // assuming bright white light color
-        frag_color = vec4(ambient + diffuse + specular, 1.0);
+        vec3 colorLinear = ambientColor +
+                           diffuseColor * lambertian * lightColor * lightPower / distance +
+                           specColor * specular * lightColor * lightPower / distance;
+        // apply gamma correction (assume ambientColor, diffuseColor and specColor
+        // have been linearized, i.e. have no gamma correction in them)
+        vec3 colorGammaCorrected = pow(colorLinear, vec3(1.0 / screenGamma));
+        // use the gamma corrected color in the fragment
+        frag_color = vec4(colorGammaCorrected, 1.0);
     }
 }
 
